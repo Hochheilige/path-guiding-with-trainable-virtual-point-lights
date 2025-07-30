77c1ce4b1631c3d2d1c9c4b82831b811de85f8c8
Hochheilige <hochheilige01@gmail.com>
2025-07-30 18:16:43 +0800

    add files to make sweep run through CLI

---
+++ train.py
@@ -0,0 +1,43 @@
+import mitsuba as mi
+mi.set_variant("cuda_ad_rgb")
+
+import wandb
+import vapl_config
+import vapl_grid
+from integrator import *
+
+if vapl_config.config.scene == "cornell box":
+    scene_dict = mi.cornell_box()
+    scene_dict['sphere'] = {
+       'type': 'sphere',
+       'radius': 0.4,
+       'center': [0, 0.2, 0],
+       'bsdf': {
+           'type': 'roughconductor',
+           'distribution': 'ggx',
+           'alpha_u': 0.5,
+           'alpha_v': 0.1
+       },
+    }
+    scene = mi.load_dict(scene_dict)
+else:
+    scene = mi.load_file(vapl_config.config.scene)
+
+def train():
+    wandb.init()
+    wandb_config = wandb.config
+    config = vapl_config.config
+    config.sweep_config = wandb_config
+    grid = vapl_grid.vapl_grid_base.create_vapl_grid(config, scene.bbox().min, scene.bbox().max)
+    loss_fn = Loss(relativeL2_luminance_tiny_cuda_nn)
+    integrator = RHSIntegrator(grid, loss_fn, True)
+    integrator.set_depth(config.depth)
+    integrator.set_config(wandb_config.vmf_axis_encoding)
+
+    for epoch in range(config.epoch):
+        integrator.epoch = epoch
+        mi.render(scene, spp=config.spp, integrator=integrator)
+        wandb.log({"loss": integrator.losses[-1].item(), "epoch": epoch})
+
+if __name__ == "__main__":
+    train()
