528cd97bfa3f5df67eed291f3ec3a6fba38ed523
Hochheilige <hochheilige01@gmail.com>
2026-02-24 00:06:11 +0800

    set up new encodings based on last sweep

--- vapl_config.py
+++ vapl_config.py
@@ -8,27 +8,28 @@ class Config(dict):
         self.__dict__ = self
 
 raw_config = {
-    "scene"     : "cornell box", # cornell box, veach_mis
+    "scene"     : "scenes/country_kitchen/scene.xml", # cornell box, veach_mis
     "mode"      : "local",       # local, wandb, sweep
     "wandb_group"  : "default",
     "sweep_config" : None,       # add to use sweep
     "run_name"     : None,       # for wandb runs
     "grid" : {
         "layout" :                    "drjit",   # regular, mlp, drjit, drjit-mlp
-        "resolution" :                 2,
-        "n_levels" :                   4,
+        "resolution" :                 16,
+        "n_levels" :                   1,
         "interpolation" :             "Nearest",   # [Nearest, Linear, Smoothstep]
         "gaussian_mean_encoding":     "eps-norm",       # [raw, eps-norm]
         "gaussian_variance_encoding": "sigmoid",  # [exp, sigmoid, softplus]
         "vmf_amplitude_encoding":     "exp",       # [relu, softplus, exp]
         "vmf_axis_encoding":          "raw", # [raw, normalize, spherical, spherical-norm]
-        "vmf_sharpness_encoding":     "softplus",  # [exp, relu, sigmoid, softplus]
+        "vmf_sharpness_encoding":     "relu",  # [exp, relu, sigmoid, softplus]
         "accumulate_gaussians" :       False,
         "accumulate_radiance" :        False,       # radiance or parameters of vapls
         "num_neighbours_to_sample":    3,          # when accumulate radiance by neigbours
-        "stochastic_interpolation":    False,
+        "stochastic_interpolation":    True,
         "stochastic_std":              0.1,
     },
+    "loss" : "relative_l2",
     # It is also possible to pass optimizer type here
     # but right now I don't see the reason to do that
     "optimizer" : {
@@ -37,7 +38,7 @@ raw_config = {
     },
     "epoch" : 501,
     "spp" : 1,
-    "depth" : 1
+    "depth" : 8
 }
 
 config = Config(raw_config)
