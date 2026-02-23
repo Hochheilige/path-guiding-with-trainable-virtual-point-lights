67395109f386ac88b83acec4c2cd1b1b2f9f8053
Pavel Ovchinnikov <hochheilige01@gmail.com>
2026-02-23 21:55:31 +0800

    update sweep code

--- train.py
+++ train.py
@@ -1,49 +1,16 @@
 import mitsuba as mi
 mi.set_variant("cuda_ad_rgb")
 
-import wandb
-import vapl_config
-import vapl_grid
-from integrator import *
+from application import Application
+from vapl_config import config
 
-if vapl_config.config.scene == "cornell box":
-    scene_dict = mi.cornell_box()
-    scene_dict['sphere'] = {
-       'type': 'sphere',
-       'radius': 0.4,
-       'center': [0, 0.2, 0],
-       'bsdf': {
-           'type': 'roughconductor',
-           'distribution': 'ggx',
-           'alpha_u': 0.5,
-           'alpha_v': 0.1
-       },
-    }
-    scene = mi.load_dict(scene_dict)
-else:
-    scene = mi.load_file(vapl_config.config.scene)
+config.mode = "sweep"
+
 
 def train():
-    wandb.init()
-    wandb_config = wandb.config
-    config = vapl_config.config
-    config.mode = "sweep"
-    config.sweep_config = wandb_config
-    grid = vapl_grid.vapl_grid_base.create_vapl_grid(config, scene.bbox().min, scene.bbox().max)
-    loss_fn = Loss(relativeL2_luminance_tiny_cuda_nn)
-    integrator = RHSIntegrator(grid, loss_fn, True)
-    integrator.set_depth(config.depth)
-    integrator.set_config(wandb_config.vmf_axis_encoding)
+    app = Application(config)
+    app.sweep()
 
-    for epoch in range(wandb_config.epoch):
-        # Re-render GT each epoch so the model sees fresh noise
-        integrator.set_train(False)
-        mi.render(scene, spp=config.spp, integrator=integrator)
-        integrator.set_train(True)
-
-        integrator.epoch = epoch
-        mi.render(scene, spp=config.spp, integrator=integrator)
-        wandb.log({"loss": integrator.losses[-1].item(), "epoch": epoch})
 
 if __name__ == "__main__":
     train()
